c161de8d31340dea177678025656bdd70496d94f
mtnvdsk <mtnvdsk@gmail.com>
2024-03-10 19:22:40 +0530

    portfolio

---
+++ js/commands.js
@@ -0,0 +1,91 @@
+// These are the variable for the direct links
+var linkedin = "https://www.linkedin.com/in/mtnvdsk";
+var github = "https://github.com/mtnvdsk";
+var email = 'mailto:mtnvdsk@gmail.com';
+var resume = 'TNVDSaikumar_Resume.pdf';
+
+
+// This content is about the "WHOIS"
+whois = [
+  "<br>",
+  "- Data Science Enthusatic ,Python & Java Developer",
+  "<br>",
+  "- Holding a Bachelor's degree in Computer Science and Engineering, harbors a deep interest in Machine Learning and Development",
+  "<br>",
+  "- Skills:",
+  "<ul>",
+  "<li> Python </li>",
+  "<li> Java </li>",
+  "<li> Git </li>",
+  "<li> AWS </li>",
+  "<li> Machine Learning </li>",
+  "<li> Linux </li>",
+  "<li> Docker </li>",
+  "</ul>",
+  "<br>",
+  "- He shares his insights and knowledge through LInkedin publications, focusing on Machine Learning, Development",
+  "<br>"
+];
+
+
+experience = [
+  "<br>",
+  "Machine Learning Intern",
+  "<ul>",
+  "<li>Indian Servers                          March 2022- May 2022</li>",
+  "</ul>",
+  "<br>",
+  "Cyber Security Analyst Intern",
+  "<ul>",
+  "<li>Andhra Pradesh Technology Services [Govt.]       Apr 2023 - Oct 2023</li>",
+  "</ul>",
+];
+
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/mtnvdsk' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github/mtnvdsk' + "</a>",
+  "<br>"
+];
+
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          About Me',
+  '<span class="command">social</span>         My Social platforms',
+  '<span class="command">projects</span>       My Open Source Projects',
+  '<span class="command">resume</span>         My Resume',
+  '<span class="command">experience</span>     My Experience',
+  '<span class="command">history</span>        You know what it is 🥱',
+  '<span class="command">email</span>          My Email address',
+  '<span class="command">clear</span>          Clear terminal',
+  "<br>",
+];
+
+banner = [
+  "<br>",
+  ". -------------------------------------------------------------------.",
+  "| [Esc] [F1][F2][F3][F4][F5][F6][F7][F8][F9][F0][F10][F11][F12] o o o| ",    
+  "|                                                                    |        ",
+  "| [`][1][2][3][4][5][6][7][8][9][0][-][=][_<_] [I][H][U] [N][/][*][-]|        ",
+  "| [|-][Q][W][E][R][T][Y][U][I][O][P][{][}] | | [D][E][D] [7][8][9]|+||        ",
+  "| [CAP][A][S][D][F][G][H][J][K][L][;]['][#]|_|           [4][5][6]|_||        ",
+  "| [^][\][Z][X][C][V][B][N][M][,][.][/] [__^__]    [^]    [1][2][3]| ||        ",
+  "| [c]   [a][________________________][a]   [c] [<][V][>] [ 0  ][.]|_||        ",
+  "`--------------------------------------------------------------------'",  
+  "+-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+-+",
+  "|T|N|V|D| |S|A||I|K|U|M|A|R| |M|",
+  "+-+-+-+-+-+-+ +-+-+-+-+-+-+-+-+-+-+",
+  "<span class=\"color2\">For a 📃 of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+  "<span class=\"color2\">Take a 😒 at My Resume to get some 💡 about me with</span> <span class=\"command\">'resume'</span><span class=\"color2\">.</span>",
+  "<br>",
+];
+
+
+projects = [
+  "<br>",
+  '1. Lung Cancer Detection using CNN <a href="https://github.com/mtnvdsk/Lung_Cancer_Detection_using_CNN.git" target="_blank">Github' + "</a>",
+  '2. Crypto Currency Price Analytics using ANN & LSTM <a href="https://github.com/mtnvdsk/Cryptocurrency_Price_Analaysis.git" target="_blank"> Github' + "</a>",
+  "<br>"
+];
